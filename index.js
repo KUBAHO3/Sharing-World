ee6d5ee625ed5bb0bdb1be5e0a656379e4d21de7
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-03-15 15:56:06 +0530

    fix linter errors

--- index.js
+++ index.js
@@ -1,67 +1,65 @@
 function ReadElement(selector) {
-    return document.querySelector(selector);
-  }
+  return document.querySelector(selector);
+}
 
 const menu = ReadElement('#menu-icon');
 const nav = ReadElement('.mobile-nav');
 const closeNav = ReadElement('.close-button');
-const speaker = ReadElement('.speaker-holder');
 
 function AddMenu(selected, modalPart, opener) {
-    return selected.addEventListener('click', () => {
-      modalPart.classList.add(opener);
-      modalPart.style.top = '0px';
-
-    });
+  return selected.addEventListener('click', () => {
+    modalPart.classList.add(opener);
+    modalPart.style.top = '0px';
+  });
 }
 
 function RemoveMenu(selected, modalPart, remover) {
-    return selected.addEventListener('click', () => modalPart.classList.remove(remover));
-  }
+  return selected.addEventListener('click', () => modalPart.classList.remove(remover));
+}
 
 AddMenu(menu, nav, 'nav-toggle');
 RemoveMenu(closeNav, nav, 'nav-toggle');
 
 const speakers = [
-    {
-        name: 'Will Smith',
-        about: 'Hollywood actor of Enterpreneaurial legal Studies at Havard Law School',
-        quote: 'Benkler studies commons-based peer production, and published his serminal book The Wealth in 2006.',
-        image: 'images/speaker1.jpg'
-    },
-    {
-        name: 'Will Smith',
-        about: 'Hollywood actor of Enterpreneaurial legal Studies at Havard Law School',
-        quote: 'Benkler studies commons-based peer production, and published his serminal book The Wealth in 2006.',
-        image: 'images/speaker1.jpg'
-    },
-    {
-        name: 'Will Smith',
-        about: 'Hollywood actor of Enterpreneaurial legal Studies at Havard Law School',
-        quote: 'Benkler studies commons-based peer production, and published his serminal book The Wealth in 2006.',
-        image: 'images/speaker1.jpg'
-    },
-    {
-        name: 'Will Smith',
-        about: 'Hollywood actor of Enterpreneaurial legal Studies at Havard Law School',
-        quote: 'Benkler studies commons-based peer production, and published his serminal book The Wealth in 2006.',
-        image: 'images/speaker1.jpg'
-    },
-    {
-        name: 'Will Smith',
-        about: 'Hollywood actor of Enterpreneaurial legal Studies at Havard Law School',
-        quote: 'Benkler studies commons-based peer production, and published his serminal book The Wealth in 2006.',
-        image: 'images/speaker1.jpg'
-    },
-    {
-        name: 'Will Smith',
-        about: 'Hollywood actor of Enterpreneaurial legal Studies at Havard Law School',
-        quote: 'Benkler studies commons-based peer production, and published his serminal book The Wealth in 2006.',
-        image: 'images/speaker1.jpg'
-    },
-]
+  {
+    name: 'Will Smith',
+    about: 'Hollywood actor of Enterpreneaurial legal Studies at Havard Law School',
+    quote: 'Benkler studies commons-based peer production, and published his serminal book The Wealth in 2006.',
+    image: 'images/speaker1.jpg',
+  },
+  {
+    name: 'Will Smith',
+    about: 'Hollywood actor of Enterpreneaurial legal Studies at Havard Law School',
+    quote: 'Benkler studies commons-based peer production, and published his serminal book The Wealth in 2006.',
+    image: 'images/speaker1.jpg',
+  },
+  {
+    name: 'Will Smith',
+    about: 'Hollywood actor of Enterpreneaurial legal Studies at Havard Law School',
+    quote: 'Benkler studies commons-based peer production, and published his serminal book The Wealth in 2006.',
+    image: 'images/speaker1.jpg',
+  },
+  {
+    name: 'Will Smith',
+    about: 'Hollywood actor of Enterpreneaurial legal Studies at Havard Law School',
+    quote: 'Benkler studies commons-based peer production, and published his serminal book The Wealth in 2006.',
+    image: 'images/speaker1.jpg',
+  },
+  {
+    name: 'Will Smith',
+    about: 'Hollywood actor of Enterpreneaurial legal Studies at Havard Law School',
+    quote: 'Benkler studies commons-based peer production, and published his serminal book The Wealth in 2006.',
+    image: 'images/speaker1.jpg',
+  },
+  {
+    name: 'Will Smith',
+    about: 'Hollywood actor of Enterpreneaurial legal Studies at Havard Law School',
+    quote: 'Benkler studies commons-based peer production, and published his serminal book The Wealth in 2006.',
+    image: 'images/speaker1.jpg',
+  },
+];
 // console.log(speakers)
-document.querySelector('.speaker-holder').innerHTML =  speakers.map( speak => `
+document.querySelector('.speaker-holder').innerHTML = speakers.map((speak) => `
 <div class="container speaker-card mt-4">
 <div class="image-holder">
     <img src=${speak.image} width="120" height="90%" alt="speaker 1">
@@ -72,38 +70,38 @@ document.querySelector('.speaker-holder').innerHTML =  speakers.map( speak => `
     <hr class="line-breaker">
     <p class="speaker-quote">${speak.quote}</p>
 </div>
-</div>`).join('')
+</div>`).join('');
 
 const programs = [
-    {
-        title: 'Director',
-        message: 'Listen to the speakers from various countries about the messeges of sharing and opening.',
-        icon: 'images/conference.png'
-    },
-    {
-        title: 'Director',
-        message: 'Listen to the speakers from various countries about the messeges of sharing and opening.',
-        icon: 'images/conference.png'
-    },
-    {
-        title: 'Director',
-        message: 'Listen to the speakers from various countries about the messeges of sharing and opening.',
-        icon: 'images/conference.png'
-    },
-    {
-        title: 'Director',
-        message: 'Listen to the speakers from various countries about the messeges of sharing and opening.',
-        icon: 'images/conference.png'
-    },
-    {
-        title: 'Director',
-        message: 'Listen to the speakers from various countries about the messeges of sharing and opening.',
-        icon: 'images/conference.png'
-    },
-   
-]
+  {
+    title: 'Director',
+    message: 'Listen to the speakers from various countries about the messeges of sharing and opening.',
+    icon: 'images/conference.png',
+  },
+  {
+    title: 'Director',
+    message: 'Listen to the speakers from various countries about the messeges of sharing and opening.',
+    icon: 'images/conference.png',
+  },
+  {
+    title: 'Director',
+    message: 'Listen to the speakers from various countries about the messeges of sharing and opening.',
+    icon: 'images/conference.png',
+  },
+  {
+    title: 'Director',
+    message: 'Listen to the speakers from various countries about the messeges of sharing and opening.',
+    icon: 'images/conference.png',
+  },
+  {
+    title: 'Director',
+    message: 'Listen to the speakers from various countries about the messeges of sharing and opening.',
+    icon: 'images/conference.png',
+  },
 
-document.querySelector('.program-holder').innerHTML =  programs.map( program => `
+];
+
+document.querySelector('.program-holder').innerHTML = programs.map((program) => `
     <div class="container p-2 ">
         <div class="d-flex align-items-center program-card p-2">
             <img src=${program.icon} width="50" height="50" alt="lecture">
@@ -111,4 +109,4 @@ document.querySelector('.program-holder').innerHTML =  programs.map( program => 
             <span class="px-1 text-white">${program.message}</span>
         </div>
     </div>
-`).join('')
+`).join('');
